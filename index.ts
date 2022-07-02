7f44a7ab6da3fa6e0751bc83f8eb3cfe5d6e5492
Jan Z <janeazy@gmail.com>
2022-07-03 09:28:28 +1000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,63 @@
-import fastify from "fastify";
 
-const server = fastify();
+import fetch from 'node-fetch'
+import fastify from 'fastify'
 
-server.get("/", async (request, reply) => {
+const server = fastify()
+
+
+server.get('/', function (request, reply) {
   reply.code(200).send({ message: "Hello world!" });
+})
+
+async function getfile(cid, gateway) {
+  console.log('getfile: ', cid, gateway)
+  let res = await fetch(gateway + cid)
+  if (res.ok) {
+    return {
+      gateway,
+      type: res.headers.get('content-type'),
+      arrBuf: await res.arrayBuffer()
+    }
+  }
+}
+
+server.get("/:cid", async (req, res) => {
+  const cid = req.params?.cid;
+  const name = req.query?.name;
+
+  // weird edge case
+  if (cid == 'favicon.ico') return
+
+  console.log('>> requesting -->', cid, name)
+
+  let file
+  let result = await Promise.any([
+    getfile(cid, "https://dweb.link/ipfs/"),
+    getfile(cid, "https://ipfs.fleek.co/ipfs/"),
+    getfile(cid, "https://gateway.ipfs.io/ipfs/"),
+    getfile(cid, "https://cloudflare-ipfs.com/ipfs/"),
+    getfile(cid, "https://cf-ipfs.com/ipfs/"),
+    getfile(cid, "https://ipfs.io/ipfs/"),
+    getfile(cid, "https://ipfs.infura.io/ipfs/"),
+  ])
+
+  if (result) {
+    console.log('result:', result);
+    file = result
+
+    //   return res.send(type);
+    res.headers({
+      'Content-Type': file.type, // uncommenting headers makes Chrome show it
+      // 'Content-disposition': `attachment; filename="${name}"`, // downloads
+      'Content-Disposition': `inline; filename="${name}"`, // shows in browser
+    })
+
+    return res.send(Buffer.from(file.arrBuf));
+  }
+
+  return res.send('nothing bub')
 });
+
 
 server.listen(process.env.PORT || 8080, "0.0.0.0", (err, address) => {
   if (err) {
@@ -13,3 +66,11 @@ server.listen(process.env.PORT || 8080, "0.0.0.0", (err, address) => {
   }
   console.log(`Server listening at ${address}`);
 });
+// module.exports = app
+// export default app
+
+// export default async (req, res) => {
+//   await app.ready();
+//   app.server.emit('request', req, res);
+// }
+
